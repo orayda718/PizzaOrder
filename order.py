e572dc59137242cabdeccda2c6a257c7d72ea278
orayda718 <122540056+orayda718@users.noreply.github.com>
2023-10-02 09:12:31 -0400

    Add files via upload

---
+++ order.py
@@ -0,0 +1,51 @@
+# Name: Orayda Shagifa
+# CS1026A Assignment 2: Pizza Ordering System
+# Description: Program takes user's pizza order, calculates the price of each pizza, tax, and total, then prints receipt
+# Due Date: 10.26.2022
+
+from pizzaReceipt import generateReceipt
+pizzaOrder = []     # entire order list
+pizza = ()          # pizza tuple, will store size and toppings
+toppings = []       # toppings list
+TOPPINGS = ('ONION', 'TOMATO', 'GREEN PEPPER', 'MUSHROOM', 'OLIVE', 'SPINACH', 'BROCCOLI', 'PINEAPPLE', 'HOT PEPPER', 'PEPPERONI', 'HAM', 'BACON', 'GROUND BEEF', 'CHICKEN', 'SAUSAGE')
+
+order1 = input("Do you want to order a pizza? ")
+
+
+# loops until user is done ordering pizzas
+while order1.upper() != "NO" and order1.upper() != "Q" and order1.upper() != "":
+
+    del pizza   # clears pizza tuple
+    toppings.clear()    # clears topping choices
+
+    size = input("Choose a size:  S, M, L, or XL: ")      # pizza size
+
+    while size.upper() != 'S' and size.upper() != 'M' and size.upper() != 'L' and size.upper() != 'XL':     # checks if size is inputted correctly
+        size = input("Choose a size:  S, M, L, or XL: ")  # replaces with correct size input
+
+    print('Type in one of our toppings to add it to your pizza. To see the list of toppings, enter "LIST". '
+          'When you are done adding toppings, enter "X"')
+    toppingInput = input("")      # pizza topping
+
+    while toppingInput.upper() != "X":  # asks for pizza topping until user enters 'x'
+
+        if toppingInput.upper() == "LIST":
+            print(TOPPINGS)     # prints list of toppings
+
+        elif toppingInput.upper() in TOPPINGS:  # checks if input is valid/in the list of toppings
+            print("Added", toppingInput.upper(), "to your pizza")
+            toppings.append(toppingInput.upper())   # adds the valid topping to the topping list
+
+        else:
+            print("Invalid topping")    # input is not one of the topping choices or 'list', topping isn't added to order
+
+        print('Type in one of our toppings to add it to your pizza. To see the list of toppings, enter "LIST". '
+              'When you are done adding toppings, enter "X"')
+        toppingInput = input("")
+
+    pizza = (size.upper(), toppings.copy())    # creates pizza tuple
+    pizzaOrder.append(pizza)    # appends the pizza to the pizza order list
+
+    order1 = input("Do you want to continue ordering? ")    # asks user if they want to order another pizza
+
+generateReceipt(pizzaOrder)     # calls function to print receipt from the pizzaReceipt.py file
---
+++ pizzaReceipt.py
@@ -0,0 +1,58 @@
+# Name: Orayda Shagifa
+# CS1026A Assignment 2: Pizza Ordering System
+# Description: Program takes user's pizza order, calculates the price of each pizza, tax, and total, then prints receipt
+# Due Date: 10.26.2022
+
+# function to print out the entire receipt
+def generateReceipt(pizzaOrder):
+    pizzaCost = 0       # base cost of pizza
+    totalCost = 0       # total cost of pizza
+    toppingCost = 0     # cost of toppings
+
+    if len(pizzaOrder) == 0:     # checks if pizza tuple is empty
+        print("You did not order anything")     # executes if user enters no or q
+
+    else:
+        print("Your order:")
+        for i in range(len(pizzaOrder)):
+            if pizzaOrder[i][0] == 'S':     # small pizza price
+                pizzaCost = 7.99
+                if len(pizzaOrder[i][1]) > 3:   # small pizza extra topping price
+                    toppingCost = 0.50
+                else:
+                    toppingCost = 0     # resets topping cost
+            elif pizzaOrder[i][0] == 'M':     # medium pizza price
+                pizzaCost = 9.99
+                if len(pizzaOrder[i][1]) > 3:   # medium pizza extra topping price
+                    toppingCost = 0.75
+                else:
+                    toppingCost = 0
+            elif pizzaOrder[i][0] == 'L':     # large pizza price
+                pizzaCost = 11.99
+                if len(pizzaOrder[i][1]) > 3:   # large pizza extra topping price
+                    toppingCost = 1.00
+                else:
+                    toppingCost = 0
+            elif pizzaOrder[i][0] == 'XL':     # extra large pizza price
+                pizzaCost = 13.99
+                if len(pizzaOrder[i][1]) > 3:   # extra large pizza extra topping price
+                    toppingCost = 1.25
+                else:
+                    toppingCost = 0
+
+            print("Pizza %d: %s 				  %.2f" % ((i + 1), pizzaOrder[i][0], pizzaCost))   # prints pizza size and base cost
+
+            for x in range(len(pizzaOrder[i][1])):  # prints toppings
+                print("-", pizzaOrder[i][1][x])
+            if toppingCost != 0:    # prints extra toppings
+                for y in (range(len(pizzaOrder[i][1]) - 3)):
+                    print("Extra Topping (%s)		   %.2f" % (pizzaOrder[i][0], toppingCost))
+
+            toppingCost *= (len(pizzaOrder[i][1]) - 3)  # calculates topping cost
+            totalCost += toppingCost + pizzaCost    # calculates total cost before tax
+
+        tax = round((totalCost * 0.13), 2)  # calculates tax
+        totalCost += tax    # calculates total cost with tax
+
+        print("Tax:					   %.2f" % tax)     # prints tax
+        print("Total:					  %.2f" % totalCost)    # prints total cost
